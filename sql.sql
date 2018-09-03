6b5076d0701151d1d523445e74c779dc61340373
Rafael Ferreira Dos Santos <rafaelsantos@brq.com>
2018-09-03 16:47:37 -0300

    sql

---
+++ sql.sql
@@ -0,0 +1,11 @@
+create table convidado(
+id integer auto_increment,
+nome varchar(100) not null,
+email varchar(100) not null,
+telefone varchar(9) not null,
+primary key (id));
+
+
+insert into convidado values(null,'Raimundo Nonato','raimundinho@gmail.com','999999999');
+insert into convidado values(null,'Pedro Martinez','pedrinho@gmail.com','888888888');
+insert into convidado values(null,'Raul Seixas','raulzinho@gmail.com','777777777');
