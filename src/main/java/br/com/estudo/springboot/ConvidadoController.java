2877c900d2143b20c7811929638eaff4950bea44
Rafael Ferreira Dos Santos <rafaelsantos@brq.com>
2018-09-04 18:52:51 -0300

    Inserindo Convidado

--- src/main/java/br/com/estudo/springboot/ConvidadoController.java
+++ src/main/java/br/com/estudo/springboot/ConvidadoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by rafaelsantos on 30/08/2018.
@@ -26,6 +28,20 @@ public class ConvidadoController {
 
         Iterable<Convidado> convidados = repository.findAll();
         model.addAttribute("convidados",convidados);
+        return "listaconvidados";
+    }
+
+    @RequestMapping(value = "salvar", method = RequestMethod.POST)
+    public String salvar(Model model, @RequestParam("nome") String nome , @RequestParam("email") String email , @RequestParam("telefone") String telefone){
+
+
+        repository.save(new Convidado(nome,email,telefone));
+
+        Iterable<Convidado> convidados = repository.findAll();
+
+        model.addAttribute("convidados",convidados);
+
+        System.out.println("Passou "+nome);
         return "listaconvidados";
     }
 }
